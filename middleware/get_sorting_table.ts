891c1c72c0e30481cfcc0fb3e9543117837cad8b
robin <ibots.robin@gmail.com>
2022-11-29 11:15:53 +0530

    Task Finished 28/11/2022

--- middleware/get_sorting_table.ts
+++ middleware/get_sorting_table.ts
@@ -12,13 +12,13 @@ const prisma = new PrismaClient();
 export const GetTableDetails = async (req: any, res: any) => {
   //req.body  //-------------> Has all request data
   const get_table_details1 = await prisma.table_sorting.findMany({});
-  var final_result = get_table_details1.map(function (element) {
+  var final_result = get_table_details1.map(function (e) {
     var results = {
-      ID: element.ID,
-      user_name: element.user_name,
-      contact_number: element.contact_number,
-      Address: element.Address,
-      profile_pic:element.profile_pic && Buffer.from(element.profile_pic).toString(),
+      ID: e.ID,
+      user_name: e.user_name,
+      contact_number: e.contact_number,
+      Address: e.Address,
+      profile_pic:e.profile_pic && Buffer.from(e.profile_pic).toString(),
     };
     return results;
   });
@@ -27,5 +27,5 @@ export const GetTableDetails = async (req: any, res: any) => {
       typeof v === "bigint" ? v.toString() : v
     )
   );
- res.status(200).json({ Result: "Image details", data: get_table });
+ res.status(200).json({ Result: "Image details", data: get_table});
 };
